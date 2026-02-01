5abf1de8bf88364ab7dc18b5d7277cd16d9ff73e
Paisley <razuberi.pastry@gmail.com>
2026-01-31 23:22:43 -0700

    Finished

--- HW3/script.js
+++ HW3/script.js
@@ -1,1 +1,15 @@
 
+console.log("Three of my favorite websites are AnimeNavi, What-Song.com, and WornonTV.net.");
+
+console.log("Three of my favorite games are Journey, Genshin Impact, and Reverse: 1999.");
+
+console.log("Three of my favorite artists are Gorillaz, Vivienne Westwood, and Hayao Miyazaki.");
+console.log("Gorillaz is culturally significant for being one of the first virtual bands, and they inspire so many other artists. Vivienne Westwood was more culturally significant before her passing when she revolutionaized the fashion industry with her movements, after her death her brand became a luxury and downgraded in quality and siginifance");
+console.log("Hayao Miyazaki is culturally significant for his contributions to animation and storytelling, creating timeless films that resonate with audiences worldwide.");
+//I included one music artist, one fashion artist, and one film artist to show a variety of art forms.
+//I could go on about these but I'm trying to keep it decently short.
+
+let welcome = prompt("What is your favorite game?");
+if (welcome !== null) {
+  console.log("Your favorite game is,, " + welcome + "? That's awesome!");
+} //When the prompt is entered, it will display the message in the console. Had to look this one up, was a bit difficult to figure out on my own.
